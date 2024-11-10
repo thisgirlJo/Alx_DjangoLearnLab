89680acb6c8066e7bfa34ffbbdefe5bc516258e9
Judith Ogbole <judithogbole71@gmail.com>
2024-11-10 13:49:39 +0100

    query set

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,4 +1,5 @@
 from .models import Author
 
 Library.objects.get(name=library_name)", "books.all()
-Author.objects.filter(name='George Orwell').values()
+Author.objects.get(name=author_name)", "objects.filter(author=author)
+

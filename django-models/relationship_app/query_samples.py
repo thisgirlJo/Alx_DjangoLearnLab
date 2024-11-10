c7907b4d2722e3b8e7ea53d9e840f0c99250b16b
Judith Ogbole <judithogbole71@gmail.com>
2024-11-10 13:52:33 +0100

    query set

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -2,4 +2,4 @@ from .models import Author
 
 Library.objects.get(name=library_name)", "books.all()
 Author.objects.get(name=author_name)", "objects.filter(author=author)
-
+Librarian.objects.get(library=library_name)

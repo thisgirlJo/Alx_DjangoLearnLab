0c3fa1972c65c06d131efdfa2fe3b76cc32828f2
Judith Ogbole <judithogbole71@gmail.com>
2024-11-21 23:48:08 +0100

    Templates & User

--- django-models/relationship_app/query_samples.py
+++
@@ -1,5 +0,0 @@
-from .models import Author
-
-Library.objects.get(name=library_name)", "books.all()
-Author.objects.get(name=author_name)", "objects.filter(author=author)
-Librarian.objects.get(library=library_name)

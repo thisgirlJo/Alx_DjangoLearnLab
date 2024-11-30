4b402285bd31f9801436b7d6620faba486fe4978
Judith Ogbole <judithogbole71@gmail.com>
2024-12-01 00:41:51 +0100

    User Profile

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,33 +1,54 @@
+"""Models"""
 from django.db import models
-#from django.contrib.auth.model import User
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name
+    def __str__(self) -> str:
+        return f"{self.name}"
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.name
+    def __str__(self) -> str:
+        return f"{self.title}"
+
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
 
-    def __str__(self):
-        return self.name
+    def __str__(self) -> str:
+        return f"{self.name}"
+
 
 class Librarian(models.Model):
     name = models.CharField(max_length=200)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.name
+    def __str__(self) -> str:
+        return f"{self.name}"
 
-""" class UserProfile(User):
-    pass """
+ROLES = (
+    ("Admin", "Admin"),
+    ("Librarian", "Librarian"),
+    ("Member", "Member"),
+)
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLES)
+    def __str__(self) -> str:
+        return f"{self.user}"
+    
+    
+# class CustomUser(AbstractUser):
+#     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     role = models.CharField(max_length=10, choices=ROLES)
+
+#     def __str__(self) -> str:
+#         return f"{self.user}"
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.views.generic import ListView, CreateView
+
+from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
 from django.urls import reverse_lazy, reverse
-from django.contrib.auth import logout
 from django.shortcuts import redirect
+from django.conf import settings
 
 # Create your views here.
 def list_books(request):
@@ -19,7 +18,7 @@ class LibraryListView(ListView, DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
 
-    def get_list_data(self):
+    def get_list_data(self, request):
         library = Library.object.get()
         format = {'library': library}
         #return format
@@ -35,16 +34,4 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
 
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/register.html'
-
-""" def custom_logout_view(request):
-    logout(request)
-
-    # Dynamically generate the logout URL
-    logout_url = reverse('register')
-
-    # Redirect to the logout URL
-    return redirect(logout_url) """
+User = settings.AUTH_USER_MODEL

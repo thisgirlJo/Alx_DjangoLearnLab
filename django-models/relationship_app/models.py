ff266c0ef2b58ca287bf048a87d103c4c2016fec
Judith Ogbole <judithogbole71@gmail.com>
2024-12-01 00:35:15 +0100

    User Profile

---
+++ django-models/relationship_app/models.py
@@ -0,0 +1,47 @@
+"""Models"""
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=200)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+ROLES = (
+    ("Admin", "Admin"),
+    ("Librarian", "Librarian"),
+    ("Member", "Member"),
+)
+
+class CustomUser(AbstractUser):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLES)
+
+    def __str__(self) -> str:
+        return f"{self.user}"
